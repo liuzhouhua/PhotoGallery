985a2850c576b83dc56225ee91d21ca6e44f6b3e
liuzhouhua <liuzhouhua@akey.me>
2015-09-11 22:14:54 +0800

    2015/9/11 01

--- PhotoGallery/src/com/example/photogallery/handler/ThumbnailDowmloader.java
+++ PhotoGallery/src/com/example/photogallery/handler/ThumbnailDowmloader.java
@@ -29,15 +29,11 @@ public class ThumbnailDowmloader<Token> extends HandlerThread {
 	//同步HashMap
 	Map<Token, String> requsetMap = Collections.synchronizedMap(new HashMap<Token, String>());
 	
-	//缓存
-	LruCache<String, Bitmap> mLruCache;
-	
 	private Bitmap bitmap=null;
 
 	public ThumbnailDowmloader(Handler responseHandler) {
 		super(TAG);
 		mResponseHandler = responseHandler;
-		LruCacheUtils();
 	}
 
 	public interface Listener<Token>{
@@ -78,14 +74,9 @@ public class ThumbnailDowmloader<Token> extends HandlerThread {
 				return;
 			}
 			
-			if(getBitmapFromLruCache(url)==null){
-				byte[] bitmapBytes = new FlickrFetcher().getUrlBytes(url);
-				bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
-				Log.i(TAG, "Bitmap created");
-				addBitmapToLruCache(url, bitmap);
-			}else{
-				bitmap = getBitmapFromLruCache(url);
-			}
+			byte[] bitmapBytes = new FlickrFetcher().getUrlBytes(url);
+			bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+			Log.i(TAG, "Bitmap created");
 			
 			
 			//返回订制的message给主线程
@@ -101,89 +92,12 @@ public class ThumbnailDowmloader<Token> extends HandlerThread {
 				}
 			});
 		}catch(IOException e){
-			Log.e(TAG, "Error downloading image", e);//
+			Log.e(TAG, "Error downloading image", e);
 		}
 	}
 
 	public void clearQueue(){
 		mHandler.removeMessages(MESSAGE_DOWNLOAD);
 		requsetMap.clear();
-	}
-	
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-	public void LruCacheUtils(){
-		int MAXMEMONRY = (int) (Runtime.getRuntime().maxMemory()/1024);
-		if(mLruCache==null){
-			mLruCache = new LruCache<String, Bitmap>(MAXMEMONRY/8){
-				@Override
-				protected int sizeOf(String key, Bitmap value) {
-					// 重写此方法来衡量每张图片的大小，默认返回图片数量。
-					return super.sizeOf(key, value);
-				}
-				
-				@Override
-				protected void entryRemoved(boolean evicted, String key,
-						Bitmap oldValue, Bitmap newValue) {
-					super.entryRemoved(evicted, key, oldValue, newValue);
-					Log.v("tag", "hard cache is full , push to soft cache");
-				}
-			};
-		}
-	}
-	
-	/*
-	 * 清空缓存
-	 * */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-	public void clearCache(){
-		if(mLruCache!=null){
-			if(mLruCache.size()>0){
-				Log.d("CacheUtils","mLruCache.size() " + mLruCache.size());
-				mLruCache.evictAll();
-				Log.d("CacheUtils","mLruCache.size() " + mLruCache.size());
-			}
-			mLruCache = null;
-		}
-	}
-	
-	/*
-	 * 添加图片到缓存
-	 * */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-	public synchronized void addBitmapToLruCache(String url,Bitmap bitmap){
-		if(mLruCache.get(url)==null){
-			if(url!=null && bitmap!=null){
-				mLruCache.put(url, bitmap);
-			}
-		}else{
-			Log.w(TAG, "the res is aready exits");
-		}
-	}
-	
-	/*
-	 * 从缓存中取图片
-	 * */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-	public synchronized Bitmap getBitmapFromLruCache(String url){
-		Bitmap bitmap = mLruCache.get(url);
-		if(bitmap!=null){
-			return bitmap;
-		}
-		return null;
-	}
-	
-	/*
-	 * 从缓存中移除
-	 * */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-	public synchronized void removeCache(String url){
-		if(url!=null){
-			if(mLruCache!=null){
-				Bitmap bitmap = mLruCache.get(url);
-				if(bitmap!=null){
-					bitmap.recycle();
-				}
-			}
-		}
 	}
 }
